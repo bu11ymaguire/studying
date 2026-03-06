87c987977cec7401ae37bd5d2e91033334762302
bu11ymaguire <jwkim628@gmail.com>
2026-03-06 15:37:18 +0900

    feat: Introduce `Value` class for automatic differentiation, implementing constructors and basic `+` and `*` operations.

---
+++ GPT/cpp/value.cpp
@@ -0,0 +1,38 @@
+#include "value.h"
+
+Value::Value(double data)
+{ //최초의 Value 생성
+    this->data = data;
+    this->grad = 0.0;
+
+    this->children = {};
+    this->local_grads = {};
+}
+
+Value::Value(double data, std::vector<Value*>children, std::vector<double> local_grads)
+{
+    this->data = data;
+    this->grad = 0.0;
+
+    this->children = children;
+    this->local_grads = local_grads;
+}
+
+Value* Value::operator+(Value* other)
+{
+   double result = this->data + other->data;
+   std::vector<double> grads = {1.0, 1.0}; //덧셈의 국소점 미분은 1.0
+   std::vector<Value*> parents = {this,other};
+
+   return new Value(result, parents, grads);
+}
+
+Value* Value::operator*(Value* other)
+{
+    double result = this->data * other->data;
+    std::vector<double> grads = {other->data, this->data}; //국소적 미분이니까 grad가 아니라 data를 가져오자!
+    std::vector<Value*> parents = {this, other};
+
+    return new Value(result, parents, grads);
+}
+
--- GPT/cpp/value.h
+++ GPT/cpp/value.h
@@ -10,6 +10,7 @@ public:
     std::vector<double> local_grads;
 
     Value(double data);
+    Value(double data, std::vector<Value*> children, std::vector<double> local_grads); //AutoGrad를 위한 생성자 오버로딩.
 
     Value* operator+(Value* other);
     Value* operator*(Value* other);
